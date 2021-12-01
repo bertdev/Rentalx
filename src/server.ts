8505e92ab8e436234f09197295974012637243ea
bert <herbertmatos10@hotmail.com>
2021-12-01 19:33:59 -0300

    feat: adiciona camada de rotas e a rota de criação de category

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ messsage: "opa amigo" });
-});
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3000, () => console.log("server started"));
