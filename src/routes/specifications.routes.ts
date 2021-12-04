6ca7f242cb73c8cd0f819475601636da1e194292
bert <herbertmatos10@hotmail.com>
2021-12-04 18:19:09 -0300

    feat: implementa useCase de createSpecification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationsController } from "../modules/cars/useCases/createSpeficitation";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.sendStatus(201);
+  return createSpecificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
