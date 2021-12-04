5d624768836a28bc04f87a5269dd7248de2595d8
bert <herbertmatos10@hotmail.com>
2021-12-04 20:25:20 -0300

    feat: implementa toda a logica para a importacao e criacao de categories usando um arquivo csv

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,45 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: CategoriesRepository) { }
+
+  loadCategories(file: Express.Multer.File): Promise {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({ name, description });
+        })
+        .on("end", () => resolve(categories))
+        .on("error", (error) => reject(error));
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.forEach(async ({ name, description }) => {
+      const categoryExists = this.categoriesRepository.findByName(name);
+      if (!categoryExists) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
