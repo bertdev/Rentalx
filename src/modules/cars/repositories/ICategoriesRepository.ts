b028e5de1514f5f1291dd943ef883fb1de47e40b
bert <herbertmatos10@hotmail.com>
2021-12-13 12:24:03 -0300

    feat: adiciona testes para o CreateCategoryUseCase verificando sua regra de negocio

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -7,7 +7,7 @@ interface ICreateCategoryDTO {
 }
 
 interface ICategoriesRepository {
-  findByName(nome: string): Promise<Category>;
+  findByName(name: string): Promise<Category>;
   list(): Promise<Category[]>;
   create({ name, description }: ICreateCategoryDTO): Promise<void>;
 }
