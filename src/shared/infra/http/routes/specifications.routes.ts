5eaab63b82596e168b42da9ea3f3aba6cc7ec6dd
bert <herbertmatos10@hotmail.com>
2021-12-14 17:46:50 -0300

    feat: adiciona o middleware de verificacao se um user logado é admin

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,11 +3,17 @@ import { Router } from "express";
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpeficitation/CreateSpecificationController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+
 const specificationsRoutes = Router();
 
 const createSpecificationsController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationsController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationsController.handle
+);
 
 export { specificationsRoutes };
