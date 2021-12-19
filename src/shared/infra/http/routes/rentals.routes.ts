35865154753731f575fcb5349df19a1d15eb7e09
bert <herbertmatos10@hotmail.com>
2021-12-19 12:10:27 -0300

    feat: implementa a funcionalidade de listagem de alugeis que um usuario ja fez

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUser = new ListRentalsByUserController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 rentalsRoutes.post(
@@ -15,5 +17,6 @@ rentalsRoutes.post(
   ensureAuthenticated,
   devolutionRentalController.handle
 );
+rentalsRoutes.get("/user", ensureAuthenticated, listRentalsByUser.handle);
 
 export { rentalsRoutes };
